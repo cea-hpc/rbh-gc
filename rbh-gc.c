b9c73e860c10f53e92e785f909211d1faa65d589
Quentin Bouget <quentin.bouget@cea.fr>
2020-11-22 18:20:15 +0100

    gc: implement open_by_id_at()
    With this commit, rbh-gc can be considered functionnal.
    
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>
    Change-Id: I8836d53337c643139843bb3d772a9a2a21901e9d

--- rbh-gc.c
+++ rbh-gc.c
@@ -12,6 +12,7 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sysexits.h>
 #include <unistd.h>
 
@@ -68,14 +69,35 @@ usage(void)
      |                          open_by_id_at()                           |
      *--------------------------------------------------------------------*/
 
+static struct file_handle *
+id2handle(const struct rbh_id *id)
+{
+    struct file_handle *handle;
+
+    handle = malloc(sizeof(*handle) + id->size - sizeof(int));
+    if (handle == NULL)
+        error(EXIT_FAILURE, errno, "malloc");
+
+    memcpy(&handle->handle_type, id->data, sizeof(int));
+    memcpy(handle->f_handle, id->data + sizeof(int), id->size - sizeof(int));
+    handle->handle_bytes = id->size - sizeof(int);
+
+    return handle;
+}
+
 static int
 open_by_id_at(int mount_fd, const struct rbh_id *id, int flags)
 {
-    (void)mount_fd;
-    (void)id;
-    (void)flags;
-    error(EX_SOFTWARE, ENOSYS, "open_by_id_at");
-    __builtin_unreachable();
+    struct file_handle *handle = id2handle(id);
+    int save_errno;
+    int fd;
+
+    /* This requires CAP_DAC_READ_SEARCH */
+    fd = open_by_handle_at(mount_fd, handle, flags);
+    save_errno = errno;
+    free(handle);
+    errno = save_errno;
+    return fd;
 }
 
     /*--------------------------------------------------------------------*
